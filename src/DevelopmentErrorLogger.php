0c387fbdba13dbd5dcba86238b6fabbf09248eca
Paul Water <p.r.water@setbased.nl>
2017-06-26 15:20:47 +0200

    Reorganized inheritance.

--- src/DevelopmentErrorLogger.php
+++ src/DevelopmentErrorLogger.php
@@ -6,17 +6,8 @@ namespace SetBased\Abc\ErrorLogger;
  * An error logger for development purposes. It will show all information of the error on the user's screen. Use this
  * error logger on development environments only.
  */
-class DevelopmentErrorLogger extends ProductionErrorLogger
+class DevelopmentErrorLogger extends CoreErrorLogger
 {
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Object constructor.
-   */
-  public function __construct()
-  {
-    // Nothing to do.
-  }
-
   //--------------------------------------------------------------------------------------------------------------------
   /**
    * Opens output.
