00ac5150feb5fb3e5667cba04cc2810452ab04e7
Paul Water <p.r.water@setbased.nl>
2023-02-05 21:51:07 +0100

    Use GitHub Actions.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -5,3 +5,6 @@ error_reporting(E_ALL | E_STRICT);
 date_default_timezone_set('Europe/Amsterdam');
 
 require __DIR__.'/../vendor/autoload.php';
+
+ini_set('zend.exception_ignore_args', false);
+ini_set('xdebug.var_display_max_depth', 100);
